6fde8cfd41b5cd06acc37dae160fda645ef2fc43
unknown <juanfarrel0404@gmail.com>
2023-07-18 00:12:28 +0700

    portfolio dengan tailwind css

---
+++ src/script.js
@@ -0,0 +1,26 @@
+//HAMBURGER
+window.onscroll = function() {
+    const header = document.querySelector('header');
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        header.classList.add("navbar-fixed");
+    } else {
+        header.classList.remove("navbar-fixed");
+    }
+ 
+}
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+})
+
+document.addEventListener("click", function(e){
+    if(!hamburger.contains(e.target)){
+        hamburger.classList.remove("hamburger-active");
+        navMenu.classList.toggle('hidden');
+    }
+})
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    fontFamily: {
+      montserrat: ['Montserrat'],
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        dark: "#1e293b",
+      },
+    },
+  },
+  plugins: [],
+}
+
